014b4bd1c1a681d35b646cd9190a80ca2100b258
Ansoumane Camara <ansoumane.camara@externe.sacem.fr>
2017-10-30 20:15:58 +0100

    Initialisation du repository Angular + SpringBoot + Maven

---
+++ src/main/java/com/association/website/webapintic/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.association.website.webapintic;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebApinticApplication.class);
+	}
+
+}
---
+++ src/main/java/com/association/website/webapintic/WebApinticApplication.java
@@ -0,0 +1,12 @@
+package com.association.website.webapintic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApinticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApinticApplication.class, args);
+	}
+}
