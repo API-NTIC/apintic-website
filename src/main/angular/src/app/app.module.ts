f3d3c0577d455898dd173ee5014d58e1f10a3db7
MEHRI Ismail <ismail.mehri@gmail.com>
2017-11-04 01:52:22 +0100

    Ajouter le header et le footer

--- src/main/angular/src/app/app.module.ts
+++ src/main/angular/src/app/app.module.ts
@@ -1,11 +1,17 @@
+import { WelcomeComponent } from './home/welcome/welcome.component';
+import { FooterComponent } from './commun/footer/footer.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent} from './commun/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/main/angular/src/app/home/welcome/welcome.module.ts
@@ -0,0 +1,20 @@
+// Angular Imports
+import { NgModule } from '@angular/core';
+
+// This Module's Components
+import { WelcomeComponent } from './welcome.component';
+
+@NgModule({
+    imports: [
+
+    ],
+    declarations: [
+        WelcomeComponent,
+    ],
+    exports: [
+        WelcomeComponent,
+    ]
+})
+export class WelcomeModule {
+
+}
